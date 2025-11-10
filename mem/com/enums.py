f5b681ff2458e6d042f08ed36f4b6117eeb8c947
BB-LLM <tangbinbinacm@163.com>
2025-11-10 10:48:25 +0800

    diary summary

--- mem/com/enums.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class MemoryType(Enum):
-    VECTOR = "vector"
-    GRAPH = "graph"
-    SUMMARY = "summary"
-    ENUMS = ["default", "vector", "graph", "summary"]
--- personality/__init__.py
+++
@@ -1,6 +0,0 @@
-from personality.tracker import PersonalityTracker
-from personality.profile import PersonalityProfile
-from personality.adjuster import PersonalityPromptAdjuster
-
-__all__ = ['PersonalityTracker', 'PersonalityProfile', 'PersonalityPromptAdjuster']
-
